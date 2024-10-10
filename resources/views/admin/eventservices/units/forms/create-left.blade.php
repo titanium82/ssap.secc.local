f5456d13b253412f6f6f41b77ccd609cf341a447
Brian Nguyen <titanium82@gmail.com>
2024-10-10 17:30:30 +0700

    Brian Nguyen

---
+++ resources/views/admin/eventservices/units/forms/create-left.blade.php
@@ -0,0 +1,22 @@
+<div class="col-12 col-md-9">
+    <div class="card">
+        <div class="row card-body">
+            <div class="col-12 mb-3">
+                <label class="form-label">@lang('Event Service Type'):</label>
+                <x-core-select name="event_service_type_id" :required="true">
+                    @foreach ($types as $type)
+                        <x-core-select-option :value="$type->id" :title="$type->name" />
+                    @endforeach
+                </x-core-select>
+            </div>
+            <!-- name -->
+            <div class="col-12">
+                <div class="mb-3">
+                    <label class="form-label">@lang('Name'):</label>
+                    <x-core-input name="name" :value="old('unit')" :required="true"
+                        :placeholder="__('Name')" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
